49455421811cfcb5ee21dff87d183b950421e34f
Omar Beltraminelli <lordbizzul@gmail.com>
2025-08-19 18:32:58 +0200

    fixing cookie issue

--- app/sites/[domain]/boxing/page.tsx
+++ app/sites/[domain]/boxing/page.tsx
@@ -3,7 +3,6 @@ import SellProductWrapper from "./sellProductWrapper";
 import { redirect } from "next/navigation";
 import { getUserContext } from "@/lib/auth-utils";
 import { createClient } from "@/utils/supabase/server";
-import DataWrapper from "./dataWrapper";
 
 // Force dynamic rendering to prevent static/dynamic conflicts
 export const dynamic = "force-dynamic";
